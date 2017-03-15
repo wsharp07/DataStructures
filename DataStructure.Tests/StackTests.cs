189af555fb7f63f0ab64ff293a883594a76b03f3
William Sharp <wsharp@ltu.edu>
2017-03-14 20:34:47 -0400

    refactoring tests to use Assert.Throws. adding a resize test

---
+++ DataStructure.Tests/StackTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Stack.Lib;
+using Xunit;
+
+namespace DataStructure.Tests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void PushToStack_Expect_EqualPop()
+        {
+            int expected = 1;
+            var stack = new WsStack<int>(2);
+            stack.Push(expected);
+            var sut = stack.Pop();
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void PushOverStack_Expect_Resize()
+        {
+            var stack = new WsStack<int>(1);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            var sut = stack.Pop();
+            Assert.Equal(3, sut);
+        }
+
+        [Fact]
+        public void InitStackZeroLength_Expect_ArgumentOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new WsStack<int>(0));
+        }
+
+        [Fact]
+        public void PopEmptyStack_Expect_InvalidOperation()
+        {
+            var stack = new WsStack<int>(1);
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+    }
+}
---
+++ Stack.Lib/WsStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Stack.Lib
+{
+    public class WsStack<T> : IStack<T>
+    {
+        private T[] _stack;
+        private int _index;
+
+        private WsStack()
+        {
+            _index = 0;
+        }
+        public WsStack(int size) : this()
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"'{nameof(size)}' must be greater than 0");
+            }
+
+            _stack = new T[size];
+        }
+        public void Push(T value)
+        {
+            if (IsStackOverflow())
+            {
+                Array.Resize(ref _stack, (_stack.Length + 1) * 2);
+            }
+
+            _stack[_index++] = value;
+
+        }
+
+        public T Pop()
+        {
+            if (_index == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+            var result = _stack[--_index];
+            _stack[_index] = default(T);
+            return result;
+        }
+
+        private bool IsStackOverflow()
+        {
+            return _index + 1 > _stack.Length;
+        }
+    }
+}

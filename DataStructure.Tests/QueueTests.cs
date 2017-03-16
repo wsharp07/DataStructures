0d8d5a2e9ae613f3883201411f82fae2d8271fb8
William Sharp <wsharp07@users.noreply.github.com>
2017-03-15 21:15:04 -0400

    first pass at a queue implementation. used a circular array, with autoresize. implemented Queue, Dequeue, and Peek (#4)

---
+++ DataStructure.Tests/QueueTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Queue.Lib;
+using Xunit;
+
+namespace DataStructure.Tests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void Enqueue_Expect_EqualDequeue()
+        {
+            var expected = 12;
+            var queue = new WsQueue<int>(2);
+            queue.Enqueue(expected);
+            var sut = queue.Dequeue();
+
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void EqueueTwo_Expect_DequeueEqualsFirst()
+        {
+            var expected = 7;
+            var queue = new WsQueue<int>(2);
+            queue.Enqueue(expected);
+            queue.Enqueue(42);
+            var sut = queue.Dequeue();
+
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void EnqueueMoreThanSize_Expect_AutoResize()
+        {
+            var queue = new WsQueue<int>(3);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Dequeue();
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+
+            var result = queue.Dequeue();
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void EnqueueAfterResize_Expect_CorrectDequeue()
+        {
+            var queue = new WsQueue<int>(1);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+
+            Assert.Equal(7, queue.Dequeue());
+            Assert.Equal(8, queue.Dequeue());
+        }
+
+        [Fact]
+        public void EnqueueTwo_Expect_CorrectCount()
+        {
+            var queue = new WsQueue<int>(1);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+
+            Assert.Equal(2, queue.Count);
+        }
+
+        [Fact]
+        public void EnqueueTwoDequeueOne_Expect_CorrectCount()
+        {
+            var queue = new WsQueue<int>(2);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Dequeue();
+
+            Assert.Equal(1, queue.Count);
+        }
+
+        [Fact]
+        public void EnqueueAndPeek_Expect_CorrectValue()
+        {
+            var expected = 7;
+            var queue = new WsQueue<int>(2);
+            queue.Enqueue(expected);
+            var sut = queue.Peek();
+
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void EnqueueAndPeek_Expect_CountCorrect()
+        {
+            var queue = new WsQueue<int>(2);
+            queue.Enqueue(7);
+            queue.Peek();
+
+            Assert.Equal(1, queue.Count);
+        }
+
+        [Fact]
+        public void InitQueueZeroLength_Expect_ArgumentOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new WsQueue<int>(0));
+        }
+
+        [Fact]
+        public void DequeueEmptyQueue_Expect_InvalidOperation()
+        {
+            var queue = new WsQueue<int>(1);
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+    }
+}
---
+++ Queue.Lib/WsQueue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Queue.Lib
+{
+    public class WsQueue<T> : IQueue<T>
+    {
+        private T[] _array;
+        private int _size;
+        private int _headIndex;
+        private int _tailIndex => (_headIndex + Count) % _size;
+
+        public int Count { get; private set; }
+
+        public WsQueue(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"'{nameof(size)}' must be greater than 0");
+            }
+
+            _size = size;
+            _array = new T[size];
+        }
+
+        public void Enqueue(T value)
+        {
+            if (Count == _size)
+            {
+                GrowQueue();
+            }
+
+            _array[_tailIndex] = value;
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            var result = _array[_headIndex];
+            _array[_headIndex] = default(T);
+
+            Count--;
+            EvaluateHead();
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            return _array[_headIndex];
+        }
+
+        private bool IsEmpty()
+        {
+            return this.Count <= 0;
+        }
+
+        private void EvaluateHead()
+        {
+            _headIndex = (_headIndex + 1) % _size;
+        }
+
+        private void GrowQueue()
+        {
+            var newSize = (_size + 1) * 2;
+            var newArray = new T[newSize];
+
+            var index = 0;
+            var prevCount = Count;
+            while (index < prevCount)
+            {
+                newArray[index] = this.Dequeue();
+                index++;
+            }
+
+            _array = newArray;
+            _headIndex = 0;
+            _size = newSize;
+            Count = prevCount;
+        }
+
+    }
+}
